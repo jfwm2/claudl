b998f934ee9d186a222176141a77e8e89138e4e9
Jean-Francois Weber-Marx <jfwm@hotmail.com>
2020-05-25 18:39:56 +0000

    Add files for basic lesson 3
    Signed-off-by: Jean-Francois Weber-Marx <jfwm@hotmail.com>

---
+++ basics/lesson003/README.md
@@ -0,0 +1,26 @@
+## claudl / basics / lesson 3 
+
+- Summary:
+  - log in and launch terminal
+  - update current repository
+  - view / edit / compile / run
+- Notions:
+  - User login/password
+  - commands: `cd`, `ls`, and `make` (`git fetch`, `git rebase` as well as `git diff` and `git statsh`)
+  - open, edit, amd compile a file with `emacs`
+  - int variables 
+
+### Linux
+- log in
+- launch terminal
+
+### Shell
+- update current repository
+- navigate to current directory
+
+### emacs
+- change, save, compile, and run
+
+### C+
+- variables introduction
+- int, char, string (used for display with cout)
---
+++ basics/lesson003/hello.cpp
@@ -0,0 +1,24 @@
+// Copyright 2020 Jean-Francois Weber-Marx
+
+#include <iostream>
+
+int main() {
+  int years{5};
+  int months = 12 * years;
+  int half = years / 2;
+
+  std::cout << "Hello, I am " << years << " years old." << std::endl;
+  std::cout << years << " years is " << months << " months." << std::endl;
+  std::cout << "Half of " << years << " is " << half << '.' << std::endl;
+
+  years++;
+
+  std::cout << "Next year, I will be " << years << " years old." << std::endl;
+  std::cout << years << " years is not " << months << " months." << std::endl;
+
+  months = 12 * years;
+  half = years / 2;
+
+  std::cout << years << " years is" << months << " months." << std::endl;
+  std::cout << "Half of " << years << " is " << half << '.' << std::endl;
+}
