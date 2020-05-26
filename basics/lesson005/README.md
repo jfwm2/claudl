b7826d7109e5a4ddb0ea01fb3d1890b19a170901
Jean-Francois Weber-Marx <jfwm@hotmail.com>
2020-05-26 09:13:06 +0000

    Add basic lesson 5
    Signed-off-by: Jean-Francois Weber-Marx <jfwm@hotmail.com>

---
+++ basics/lesson005/README.md
@@ -0,0 +1,5 @@
+## claudl / basics / lesson 3 
+
+- iostream (cout, cin, endl)
+- '\n' and '\t'
+- Return code
---
+++ basics/lesson005/hello.cpp
@@ -0,0 +1,21 @@
+// Copyright 2020 Jean-Francois Weber-Marx
+
+#include <iostream>  // this provides cin, cout, and endl
+
+int main() {
+  std::cout << "How old are you?" << std::endl; 
+
+  int years{};
+
+  std::cout << "Enter you age in years: ";
+  std::cin >> years;
+  
+  std::cout << "Hello, now you are \t" << years << " years old.\n";
+  
+  years++;
+
+  std::cout << "Next year, you will be " << '\t'
+	    << years << " years old." << '\n';
+
+  return 0;  // if there is no error, we return 0
+}
