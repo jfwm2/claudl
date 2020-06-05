08f65a036a2fee6ed6edcd0fbb811bf42eefb2e7
Jean-Francois Weber-Marx <jfwm@hotmail.com>
2020-06-05 13:57:53 +0000

    Add basic lesson 13
    Signed-off-by: Jean-Francois Weber-Marx <jfwm@hotmail.com>

---
+++ basics/lesson013/README.md
@@ -0,0 +1,3 @@
+## claudl / basics / lesson 13 
+
+- for loop
---
+++ basics/lesson013/main.cpp
@@ -0,0 +1,11 @@
+// Copyright 2020 Jean-Francois Weber-Marx
+
+#include <iostream>
+
+int main() {
+  for (int i = 0; i < 5; ++i) {
+    std::cout << i << ' ';
+  }
+  std::cout << '\n';
+  return 0;
+}
